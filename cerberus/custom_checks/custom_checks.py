f171d8d508b2dbf6bee7af04a4825d771fbc7e41
BillyLi <billy.li@gov.bc.ca>
2023-08-28 17:09:30 -0700

    solve comments

--- cerberus/custom_checks/custom_checks.py
+++ cerberus/custom_checks/custom_checks.py
@@ -93,7 +93,6 @@ def check_PV():
 
 def check_kyverno():
     logging.info("Check if the Kyverno ConfigMap is okay.")
-    result = False
 
     create_config = runcommand.invoke(
         'oc create -n openshift-bcgov-cerberus configmap simple-test --from-literal=data="asdf" && echo "successed"')
