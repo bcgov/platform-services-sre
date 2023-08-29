37373bce29ecbd83b62abde2564e8791a924396d
Rajpal Chauhan <rajpal.chauhan@gov.bc.ca>
2023-08-29 09:44:01 -0700

    Scipt updated as per feedback
    Signed-off-by: Rajpal Chauhan <rajpal.chauhan@gov.bc.ca>

--- uptime/monitor-setup/uptime-rc-webhook.js
+++ uptime/monitor-setup/uptime-rc-webhook.js
@@ -1,14 +1,14 @@
 /**
  * uptime-rc-webhook.js
  * Add Uptime.com notifications via WebHook in Rocket.Chat
- * Usage on uptime.com webhook integration setup: put this in the Custom HTTP Headers to tag ppl in the messagecontactusers: @<user1_rocketchat_account> @<user2_rocketchat_account> ... ,
+ * Usage on uptime.com webhook integration setup: put this in the Custom HTTP Headers to tag ppl in the message`contactusers: @<user1_rocketchat_account> @<user2_rocketchat_account> ... ,`
  */
 
-/ globals console, _, s /
+/* globals console, _, s */
 const USERNAME = 'Uptime.com Alerts';
 const AVATAR_URL = 'https://avatars.githubusercontent.com/u/54849620?s=200&v=4';
 
-const time = (totalSeconds) => {
+  const time = (totalSeconds) => {
   const seconds = Math.floor(totalSeconds % 60);
   const totalMinutes = Math.floor(totalSeconds / 60);
   const minutes = totalMinutes % 60;
@@ -19,12 +19,12 @@ const time = (totalSeconds) => {
   return `${days}d:${hours}h:${minutes}m:${seconds}s`;
 }
 
-/ exported Script /
-class Script {
+/* exported Script */
+  class Script {
   /**
    * @params {object} request
    */
-  process_incoming_request({ request }) {
+    process_incoming_request({ request }) {
     const data = request.content.data;
     // We are using Custom HTTP Headers to add user tagging information:
     const contactUser = request.headers.contactusers;
@@ -33,8 +33,8 @@ class Script {
     let statusText = `DOWN`;
     let isUp = data.alert.is_up;
     if (isUp) {
-      attachmentColor = `#36A64F`;
-      statusText = `UP`;
+     attachmentColor = `#36A64F`;
+     statusText = `UP`;
     }
 
     let attachmentText = '';
@@ -52,17 +52,17 @@ class Script {
     }
 
     return {
-      content:{
-	alias: USERNAME,
-	icon_url: AVATAR_URL,
-	text: `${contactUser} Monitor ${data.service.name} is ${statusText}.\n Link: ${data.account.site_url}\n`,
-	attachments: [{
-	  title: titleText,
-	  title_link: titleLink,
-	  text: attachmentText,
-	  color: attachmentColor
-	}]
-      }
-    };
+     content:{
+	    alias: USERNAME,
+	    icon_url: AVATAR_URL,
+	    text: `${contactUser} Monitor ${data.service.name} is ${statusText}.\n Link: ${data.account.site_url}\n`,
+	    attachments: [{
+	     title: titleText,
+	     title_link: titleLink,
+	     text: attachmentText,
+	     color: attachmentColor
+	    }]
+     }
+   };
   }
 }
